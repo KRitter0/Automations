719c6122a91333e57897977013f239523291016e
KRitter0 <kristoffritter@gmail.com>
2025-12-01 17:46:43 +0100

    Updated app-build-manifest.json

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("096fb7065f26fc44")
+/******/ 		__webpack_require__.h = () => ("9acebdd3e630a459")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0f27ca46c56ff9f0"; }
+/******/ 		__webpack_require__.h = function() { return "5e333c7edda84ce6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
